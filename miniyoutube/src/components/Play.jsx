48159044b96be577de807aa188b596d7417bf3a4
heon98 <duafogjs@naver.com>
2023-01-06 14:37:00 +0900

    add "Play.jsx"

--- miniyoutube/src/components/Play.jsx
+++ miniyoutube/src/components/Play.jsx
@@ -4,30 +4,43 @@ const Play = props => {
     const [video, setVideo] = useState([]);
     useEffect(() => {
         fetch(
-            "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBeymQmQdVEAPAciY51spK3v0kvJM-Um4o"
+            "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=KR&key=AIzaSyBeymQmQdVEAPAciY51spK3v0kvJM-Um4o"
         )
             .then((response) => response.json())
             .then((data) => {
-                const videos = data;
+                const videos = data.items;
                 setVideo(videos);
+                console.log(videos);
 
-                console.log(video);
             });
     }, []);
 
     return (
-        <iframe
-            id="ytplayer"
-            type="text/html"
-            width="720"
-            height="405"
-            src="https://www.youtube.com/embed/_uS8QRe2qfE"
-            // src={`https://www.youtube.com/embed/${video.items[0].id}`}
-            // src={`https://www.youtube.com/embed/_uS8QRe2qfE`}
-            frameborder="0"
-            allowfullscreen="allowfullscreen"
-        ></iframe>
-      )
+
+        <div className='row'>
+
+            <div className='row__posters'>
+                {video.map(video =>
+                    <iframe
+                        id="ytplayer"
+                        type="text/html"
+                        width="720"
+                        height="405"
+                        // src="https://www.youtube.com/embed/_ZAgIHmHLdc"
+                        src={`https://www.youtube.com/embed/${video?.id}`}
+                        // src={`https://www.youtube.com/embed/6LmO6rmDW08`}
+                        frameBorder="0"
+                        allowFullScreen="allowfullscreen"
+                    >
+                    </iframe>
+                )}
+            </div>
+        </div>
+
+
+
+
+    )
 }
 
 export default Play
